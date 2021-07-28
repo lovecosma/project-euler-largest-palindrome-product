7df9adc9293b5be2165441403601fdf94c6cda58
Matthew Williams <love.cosma@gmail.com>
2021-07-28 16:23:55 -0600

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,43 @@
 # Implement your procedural solution here!
+
+
+def largest_palindrome_product
+
+    num1 = 999
+    num2 = 999
+    prod = num1 * num2
+    target = findNearestPalindrome(prod)
+    
+    until target % num1 == 0 && !(target / num1 > 1000)
+        num1 -= 1
+        if(num1 < 100)
+            target = findNearestPalindrome(target)
+           num1 = 999
+        end 
+    end 
+
+
+
+
+    return target
+
+end 
+
+def findNearestPalindrome(num)
+    if isPalindrome(num)
+        num -= 1
+    end 
+    while !isPalindrome(num)
+        num -= 1
+    end 
+    return num
+end 
+
+def isPalindrome(num)
+    string_num = num.to_s
+    if(string_num == string_num.reverse)
+        true
+  else
+        false
+  end 
+end 
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,46 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct 
+
+    def answer
+
+        num1 = 999
+        target = findNextPalindrome(num1 * 999)
+
+        until target % num1 == 0 && !(target / num1 > 999)
+
+            if(num1 < 100)
+                num1 = 999
+                target = findNextPalindrome(target)
+            end 
+            num1 -= 1
+
+        end 
+        
+
+        target
+
+    end 
+
+    def isPalindrome?(num)
+        string_num = num.to_s
+        if(string_num == string_num.reverse)
+            true
+        else
+            false
+        end  
+    end 
+
+    def findNextPalindrome(num)
+
+        if isPalindrome?(num)
+            num -= 1
+        end 
+
+        while !isPalindrome?(num)
+            num -= 1
+        end 
+
+        return num
+    end 
+
+end 
